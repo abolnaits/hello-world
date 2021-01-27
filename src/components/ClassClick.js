e43bb0d7b5d474951687a1af2aa5f9cef6da17e1
Andres Benitez <abolnaits@hotmail.com>
2021-01-27 12:51:04 -0500

    Event Binding

--- src/components/ClassClick.js
+++ src/components/ClassClick.js
@@ -3,14 +3,35 @@ import {Component} from 'react';
 
 class ClassClick extends Component{
     //this.props 
+    constructor(){
+        super();
+        this.state = {
+            message : 'Hi!'
+        }
 
+        //Binding
+        //this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    /*
     clickHandler(){
         console.log('Click ==> ');
+        console.log(this);
+        this.setState({
+            message:'Bye!'
+        });
     }
-    
+    */
+
+    clickHandler = () =>{
+        this.setState({
+            message:'Bye!'
+        });
+    }
     render(){
         return(
             <div>
+                <h1>{this.state.message}</h1>
                 <button onClick={this.clickHandler}>Click</button>
             </div>
         );
